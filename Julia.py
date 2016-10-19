9903adcd4d4a15b78deee91ad31728697c626374
Derwin McGeary <derwin.mcgeary@cloudwick.com>
2016-10-19 11:11:00 +0200

    Add memberness method to Julia.py, try to improve efficiency of calculations

--- Julia.py
+++ Julia.py
@@ -4,12 +4,17 @@ class set:
     def __init__(self,comp):
         self.c = comp
 
-    def isMember(self,z,steps=1000):
+    def nextIteration(self,z):
+        r = z.r
+        i = z.i
+        return Complex.Number(r*r - i*i + self.c.r, self.c.i  + 2 * r * i)
+    
+    def isMember(self,z,steps=255):
         outOfBounds = steps
         for i in xrange(steps):
             z = z.squared().add(self.c)
-            print z.magnitude()
-            if (z.magnitude() > 1000)|(z.magnitude()==0):
+#            print z.magnitude()
+            if (z.magnitude() > 10)|(z.magnitude()==0):
                 outOfBounds = i
                 break
 
@@ -18,3 +23,14 @@ class set:
         else:
             return True
 
+    def memberness(self,z,steps=25):
+        outOfBounds = steps
+        for i in xrange(steps):
+            z = self.nextIteration(z)
+#            z = z.squared().add(self.c)
+#            print z.magnitude()
+            if (z.i**2 + z.r**2 > 9):
+                outOfBounds = i
+                break
+
+        return outOfBounds*10
