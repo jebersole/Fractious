cbe852603948a8d266ec5152c3c6e8345198bdad
jebersole <ebersolej@.gmail.com>
2016-10-29 02:30:58 +0500

    Add more arguments

--- renderertest.py
+++ renderertest.py
@@ -3,32 +3,19 @@
 import unittest
 import os.path
 import struct
-import argparse
 import Renderer
+import Arguer
 
 class RendererTest(unittest.TestCase):
     # Tests if png was created and if output and input dimensions match
     def testFile(self): # 3 test cases: small, medium, and large images
         filenames = {"test1.png":"300x300", "test2.png":"50x50", "test3.png":"1000x1000"}
         for current in filenames:
-            args = self.getArgs(['julia', current, '--dimensions', filenames[current]])
-            r = Renderer.render(args).draw()
+            args = Arguer.Argue(['julia', current, '--dim', filenames[current]])
+            r = Renderer.Render(args).draw()
             self.assertTrue(os.path.isfile(current))
             self.assertTrue(self.checkDims(filenames[current][: filenames[current].index('x')],
                 filenames[current][filenames[current].index('x') + 1:], current))
-
-    def getArgs(self, argsin):
-        parser = argparse.ArgumentParser(description='Create a fractal.')
-        parser.add_argument('fractal', metavar='Fr', type=str,
-                            help='fractal to be created, e.g. julia')
-        parser.add_argument('file', metavar='F', type=str,
-                            help='filename for desired png image, e.g. file.png')
-        parser.add_argument('--dimensions', metavar='D', type=str, default='300x300',
-                            help='dimensions of image, e.g. widthxheight: 400x400')
-        parser.add_argument('--complex', metavar='C', type=str, default='[0,0.8]',
-                            help='complex number with which to draw fractal, e.g. [0,0.8]')
-        args = parser.parse_args(argsin)
-        return args
 
     def checkDims(self, twidth, theight, filename): # test input width and height
         f = open(filename, 'rb')
