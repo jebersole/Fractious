051296c7ee3e51c01bb108e165da56b97cb9cb56
Derwin McGeary <Derwin.McGeary.ext@netent.com>
2017-02-26 14:48:47 +0000

    add setPixel and drawLine functions

--- KochSnowflake.py
+++ KochSnowflake.py
@@ -12,3 +12,42 @@ class set:
             return True, smoothColor
         else:
             return False, None
+
+
+    def drawLine(self, startX, startY, endX, endY):
+
+        # TODO: actually implement drawVerticalLine
+        if endX==startX:
+            drawVerticalLine(startX, startY, endY)
+
+        if endX < startX:
+            startX, endX = endX,startX
+            startY, endY = endY, startY
+
+        slope = (endY * 1.0 - startY)/(endX * 1.0 - startX)
+
+        if slope > 1:
+            if endY < startY:
+                step = -1
+            else:
+                step = 1
+            for y in range(startY,endY, step):
+                x = Math.round(startX + (y - startY)/slope)
+                setPixel(x, y)
+        else: # slope >= 1
+            for x in range(startX,endX):
+                y = Math.round(startX + slope * (x - startX))
+                setPixel(x, y)
+            
+    def drawVerticalLine(self,startX, startY, endY):
+        if endY < startY:
+            step = -1
+        else:
+            step = 1
+        for y in range(startY,endY, step):
+                setPixel(startX, y)
+
+    def setPixel(self,x,y):
+        self.matrix[x][y] = 1
+
+        
